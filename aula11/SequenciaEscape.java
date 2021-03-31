6d6761d1584ea81a160cf58b74a6903231635d0c
Josias Salermo <josiassalermojunior@gmail.com>
2021-03-31 10:13:24 -0300

    SequenciaEscape

---
+++ aula11/SequenciaEscape.java
@@ -0,0 +1,14 @@
+pachage aula11;
+
+public class SequenciaEscape{
+
+    public static void main(String[] args){
+
+        System.out.print("\"Hello,World!\" \n\r");
+        // é o mesmo que o System.out.println();
+
+        // 1/4
+        System.out.println("1\\4");
+    }
+
+}
