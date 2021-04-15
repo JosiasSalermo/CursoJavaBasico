a024093a9d04c9323eef98efc3a1ee18078970ee
Josias Salermo <josiassalermojunior@gmail.com>
2021-04-15 09:26:54 -0300

    variaveis char

--- aulasCursoJavaBasico/src/aula11/VariaveisChar.java
+++ aulasCursoJavaBasico/src/aula11/VariaveisChar.java
@@ -1,17 +1,17 @@
 package aula11;
 
 public class VariaveisChar {
-
-    public static void main(String[] args){
-
+    
+    public static void main(String[] args) {
+        
         char o = 'o';
         char i = 'i';
 
         System.out.println("o + i");
         System.out.println("" + o + i);
+        System.out.println(o + i);
 
         char interrogacao = 0x3f;
-
         System.out.println("" + o + i + interrogacao);
     }
 }
