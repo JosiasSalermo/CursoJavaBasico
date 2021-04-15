81d969e1b80d371935e5989e8259d49154a12ad3
Josias Salermo <josiassalermojunior@gmail.com>
2021-04-15 09:34:02 -0300

    sequencia escape

---
+++ aulasCursoJavaBasico/src/aula11/SequenciaEscape.java
@@ -0,0 +1,13 @@
+package aula11;
+
+public class SequenciaEscape {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("\"Hello, World!\" \n\r");
+        // é o mesmo que println
+
+        // 1/4
+        System.out.println("1\\4");
+    }
+}
