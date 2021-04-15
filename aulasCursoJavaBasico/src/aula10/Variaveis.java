ffe8200124af898455cd27f016632ea24c2d373b
Josias Salermo <josiassalermojunior@gmail.com>
2021-04-15 09:11:45 -0300

    aula 10

--- aulasCursoJavaBasico/src/aula10/Variaveis.java
+++ aulasCursoJavaBasico/src/aula10/Variaveis.java
@@ -1,25 +1,27 @@
-
 public class Variaveis {
+    
 
-    public static void main(String[] args){
-
-        // convenção Java
+    public static void main(String[] args) {
+        
+        // Convenção Java
         int idade = 20;
         String nome = "Loiane";
         String nomeDoMeuCachorro = "totô";
         String ano = "2021";
 
+        // Reatribuir a variável
         idade = 25;
 
         // aceito, mas não utilizado
         int _idade;
         int $idade;
 
-        // não é convenção Java
+        // Não é convenção Java
         String nome_do_meu_cachorro;
-        String NomeDoMeuCachorro;
 
-        System.out.println("Idade -" + idade);
+        String NomeDoMeuCachorro; /* iciaiar com Maiúscula só Classe */
+
+        System.out.println("Idade - " + idade);
         System.out.println("Nome - " + nome);
 
         // má prática
