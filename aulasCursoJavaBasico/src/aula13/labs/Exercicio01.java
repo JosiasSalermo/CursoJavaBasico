38f49b2f828c31b71693d7a69ac27f457a51a0ff
Josias Salermo <josiassalermojunior@gmail.com>
2021-04-26 10:06:11 -0300

    git ignore

---
+++ aulasCursoJavaBasico/src/aula13/labs/Exercicio01.java
@@ -0,0 +1,12 @@
+package aula13.labs;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        
+        // Faça um Programa que mostre a mensagem 
+        // "Olá mundo" na tela.
+
+        System.out.println("Olá Mundo!");
+    }
+}
