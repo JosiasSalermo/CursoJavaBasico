993e8d3e04419aff328e460e2ddf0bfeb66556e3
Josias Salermo <josiassalermojunior@gmail.com>
2021-05-06 15:28:31 -0300

    aula 06

--- aulasCursoJavaBasico/src/aula13/labs/Exercicio05.java
+++ aulasCursoJavaBasico/src/aula13/labs/Exercicio05.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Exercicio05 {
 
     public static void main(String[] args) {
-        // Faça um Programa que converta metros para centímetro
+        // xFaça um Programa que converta metros para centímetro x
 
-       Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-       System.out.println("Entre com a quantidade de metros");
-       double metros = scan.nextDouble();
+        System.out.println("Entre com a quantidade de metros");
+        double metros = scan.nextDouble();
 
-       // 1 m = 100 cm
-       double cm = metros * 100;
+        // 1 m = 100 cm
+        double cm = metros * 100;
 
-       System.out.println(metros + " m é igual a " + cm + " cm");
+        System.out.println(metros + " m é igual a " + cm + " cm");
     }
 }
