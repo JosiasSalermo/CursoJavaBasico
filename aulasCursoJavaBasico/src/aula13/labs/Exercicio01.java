be878b84bdba1ce0596af42574babb772b50fa66
Josias <josiassalermojunior@gmail.com>
2021-05-21 18:35:53 -0300

    arrumando repositório

--- aulasCursoJavaBasico/src/aula13/labs/Exercicio01.java
+++
@@ -1,8 +0,0 @@
-public class Exercicio01 {
-    
-    public static void main(String[] args) {
-        // Faça um Programa que mostre a mensagem "Alo Mundo"
-
-        System.out.println("Alo Mundo!");
-    }
-}
