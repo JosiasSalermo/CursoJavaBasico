6ceaaaad2dd2674bf4344329eaa6a8a6a33a4cd3
Josias <josiassalermojunior@gmail.com>
2021-05-24 09:55:04 -0300

    exercicio 02

---
+++ Aulas/src/aula24/labs/Lampada.java
@@ -0,0 +1,20 @@
+package aula24.labs;
+
+public class Lampada {
+	
+	// Atributos
+	int potencia;
+	int garantiaMeses;
+	int itemIncluso;
+	int referenciaModelo;
+	String modelo;
+	String tensao;
+	String cor;
+	String tipoluz;
+	String[] telefones;	
+	boolean tipoAbajur;
+	boolean tipoLampeao;
+	boolean tipoLuminaria;
+	boolean tipoPendente;
+	
+}
---
+++ Aulas/src/aula24/labs/Livro.java
@@ -0,0 +1,10 @@
+package aula24.labs;
+
+public class Livro {
+	
+	String nome;
+	String autor;
+	String isbn;
+	int qtdPaginas;
+	int anoLancamento;
+}
