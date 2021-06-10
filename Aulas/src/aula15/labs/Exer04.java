bc18a92b98fcfce1352ce42f531659f717910623
Josia Salermo <josiassalermojunior@gmail.com>
2021-06-10 09:24:44 -0300

    exercicio 04

--- Aulas/src/aula15/labs/Exer04.java
+++ Aulas/src/aula15/labs/Exer04.java
@@ -1,10 +1,28 @@
+// Faça um Programa que verifique se uma letra digitada é 
+// vogal ou consoante
+
 package aula15.labs;
+
+import java.util.Scanner;
 
 public class Exer04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com uma letra:");
+		String letra = scan.next();
+		
+		if(letra.equalsIgnoreCase("a") ||
+				letra.equalsIgnoreCase("e") || 
+				letra.equalsIgnoreCase("i") || 
+				letra.equalsIgnoreCase("o") ||
+				letra.equalsIgnoreCase("u")) {
+			System.out.println("vogal");
+		}else {
+			System.out.println("consoante");
+		}
 	}
 
 }
