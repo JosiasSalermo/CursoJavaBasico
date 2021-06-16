2ee759139019e02e16bd81c7f235f49b001c43b9
Josia Salermo <josiassalermojunior@gmail.com>
2021-06-16 08:21:52 -0300

    aula 11 variaveis Ponto Flutuante

--- Aulas/src/aula11/VariaveisPontoFlutuante.java
+++ Aulas/src/aula11/VariaveisPontoFlutuante.java
@@ -3,7 +3,12 @@ package aula11;
 public class VariaveisPontoFlutuante {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double valorPassagem = 2.90;
+		float valorTomate = 3.95f;
+		
+		System.out.println("Valor da passagem = " + valorPassagem);
+		System.out.println("Valor do tomate = " + valorTomate);
 
 	}
 
