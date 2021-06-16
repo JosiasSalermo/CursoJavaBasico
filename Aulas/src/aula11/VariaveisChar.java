307cd501f65d209e3b615cad943dec2cf6859c07
Josia Salermo <josiassalermojunior@gmail.com>
2021-06-16 08:22:45 -0300

    aula 11 Variaveis com char

---
+++ Aulas/src/aula11/VariaveisChar.java
@@ -0,0 +1,18 @@
+package aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		char o = 'o'; // 111
+		char i = 'i'; // 105
+		
+		System.out.println(o + i);
+		System.out.println("" + o + i);
+		
+		char interrogacao = 0x3f;
+		System.out.println("" + o + i + interrogacao);
+
+	}
+
+}
