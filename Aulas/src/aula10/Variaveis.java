38058997ad2993a02a25dcfede46113b08e85c00
Josia Salermo <josiassalermojunior@gmail.com>
2021-06-16 08:20:53 -0300

    aula 10

--- Aulas/src/aula10/Variaveis.java
+++ Aulas/src/aula10/Variaveis.java
@@ -3,7 +3,30 @@ package aula10;
 public class Variaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// convenção Java
+		int idade = 20;
+		String nome = "Loiane";
+		String nomeDoMeuCachorro = "totô";
+		String ano = "2021";
+		
+		idade = 25;
+		
+		// aceito, mas não utilizado
+		int _idade;
+		int $idade;
+		
+		// não é convenção Java
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		
+		System.out.println("Idade - " + idade);
+		System.out.println("Nome - " + nome);
+		
+		// má prática
+		int a = 10;
+		String b = "Loiane";
+		
 
 	}
 
