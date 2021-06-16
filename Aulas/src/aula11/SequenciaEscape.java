a1cfdcd23e24fd06845e0a39592e135532401633
Josia Salermo <josiassalermojunior@gmail.com>
2021-06-16 08:31:54 -0300

    aula 11 Sequencia escape

---
+++ Aulas/src/aula11/SequenciaEscape.java
@@ -0,0 +1,14 @@
+package aula11;
+
+public class SequenciaEscape {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\"Hello, World!\"\n\r");
+		// System.out.println();
+		
+		System.out.println("1\\4");
+
+	}
+
+}
