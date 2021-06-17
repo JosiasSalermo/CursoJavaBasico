ebae605f52a527739ef7664c52199483db741a09
Josia Salermo <josiassalermojunior@gmail.com>
2021-06-17 08:37:43 -0300

    exer 01

---
+++ Aulas/src/aula13/labs/Exer01.java
@@ -0,0 +1,14 @@
+// Faça um Programa que mostre a mensagem 
+// "Alô mundo na tela"
+
+package aula13.labs;
+
+public class Exer01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alô Mundo!");
+
+	}
+
+}
