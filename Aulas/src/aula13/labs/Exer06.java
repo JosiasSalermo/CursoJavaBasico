fb48c14ba70912d7c022b686ef6731095299033d
Josia Salermo <josiassalermojunior@gmail.com>
2021-06-18 09:03:54 -0300

    exercicio 06

---
+++ Aulas/src/aula13/labs/Exer06.java
@@ -0,0 +1,23 @@
+// Faça um Programa que peça o raio de um círculo
+// calcule e mostre sua área
+
+package aula13.labs;
+
+import java.util.Scanner;
+
+public class Exer06 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o raio do círculo: ");
+		double raio = scan.nextDouble();
+		
+		double area = Math.PI * Math.pow(raio, 2);
+		
+		System.out.println("A área do círculo é: " + area);
+
+	}
+
+}
