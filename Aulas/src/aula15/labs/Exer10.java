7f513dc2321fee3f310cc1ed184561ed195e3a38
Josias Salermo <josiassalermojunior@gmail.com>
2021-07-01 09:22:07 -0300

    exercicio 10

---
+++ Aulas/src/aula15/labs/Exer10.java
@@ -0,0 +1,30 @@
+/* Faça um Programa que pergunte em que turno você estuda.
+ * Peça para digitar M-matutino ou V-vespertino ou N-noturno
+ * Imprima a mensagem "Bom dia!", "Boa tarde!" ou "Boa Noite"
+ * ou "Valor inválido!", conforme o caso.*/
+
+package aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer10 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o turno que você estuda:");
+		String turno = scan.next();
+		
+		switch(turno) {
+		case "m":
+		case "M": System.out.println("Bom dia!"); break;
+		case "v":
+		case "V": System.out.println("Boa tarde!");break;
+		case "n":
+		case "N": System.out.println("Boa noite!");break;
+		default: System.out.println("Valor inválido");
+		}
+	}
+
+}
