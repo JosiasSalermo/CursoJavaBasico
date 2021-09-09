7a487353539ac07336c473c0287a1d7646d5c553
Josias Salermo <josiassalermojunior@gmail.com>
2021-09-09 11:22:37 -0300

    exercicio 21

---
+++ Aulas/src/aula15/labs/Exer21.java
@@ -0,0 +1,51 @@
+package aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer21 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com a quantidade de litros vendidos");
+		double litros = scan.nextDouble();
+		
+		System.out.println("Entre com o tipo de combustível");
+		System.out.println("'A' p/ álcool ou 'G' p/ gasolina");
+		String tipo = scan.next();
+		
+		double precoGas = 2.5;
+		double precoAlc = 1.9;
+		int percDesconto = 0;
+		double total = 0;
+		double totalDesc = 0;
+		
+		if(tipo.equalsIgnoreCase("a")) {
+			
+			if(liros <= 20) {
+				percDesconto = 3;
+			}else {
+				percDesconto = 5;
+			}
+			
+			total = litros * precoAlc;
+		}else if(tipo.equalsIgnoreCase("g")) {
+			
+			if(litros <= 20) {
+				percDesconto = 4;
+			}else {
+				percDesconto = 6;
+			}
+			
+			total = litros * precoGas;
+		}
+		
+		totalDesc = (total/100) * percDesconto;
+		double precoApagar = total - totalDesc;
+		System.out.println("Valor a ser pago:" + precoApagar);
+		
+
+	}
+
+}
